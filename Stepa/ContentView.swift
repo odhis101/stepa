c1ddb3af444889cd84734538fe2ef35020a90ebf
odhis101 <joshodhiambo5@gmail.com>
2024-02-01 10:27:25 +0300

    Initial Commit

---
+++ Stepa/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stepa
+//
+//  Created by Joshua on 2/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Stepa/StepaApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepaApp.swift
+//  Stepa
+//
+//  Created by Joshua on 2/1/24.
+//
+
+import SwiftUI
+
+@main
+struct StepaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
